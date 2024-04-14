203b5df5fdcbd8c4deb125635aefede81d785bbd
MozartSoares <mozartarmstrong@gmail.com>
2024-04-14 18:15:59 -0300

    -add post to django admin

--- blog/admin.py
+++ blog/admin.py
@@ -2,3 +2,12 @@ from django.contrib import admin
 
 # Register your models here.
 from .models import Post
+
+class PostAdmin(admin.ModelAdmin):
+  list_display = ('title', 'created_on', 'slug', 'status')# exibe lista com todos os posts criados
+  list_filter = ('status',)  # filtra os posts por status 
+  search_fields = ['title', 'content']# campos de busca
+  prepopulated_fields = {"slug":('title',)}# campos que são prepopulados
+  
+  
+admin.site.register(Post, PostAdmin)
