642afd9d6ea36835157c4a9dbabc49e973cf071e
Mozart Soares <mozartarmstrong@gmail.com>
2024-04-19 20:05:07 -0300

    -add and configured templates

--- blog/views/post_view.py
+++ blog/views/post_view.py
@@ -1,7 +1,14 @@
-from django.http import HttpResponse
 from django.views import generic
 
+from blog.models import Post
+
 #herdando de uma classe pré pronta no django para dar overwrite e modificar um método, nesse caso o get
-class PostView(generic.View):
-  def get(self, request, *args, **kwargs):
-    return HttpResponse('hello world !')
+class PostView(generic.ListView):
+  #criando a lista e passando ela para o template index.html onde temos o laço for nos posts
+  queryset = Post.objects.filter(status=1).order_by('-created_on')
+  template_name= 'index.html'
+
+class PostDetail(generic.DetailView):
+  #passando o post para o post_detail para exibir atributos dele
+  model = Post
+  template_name = 'post_detail.html'
